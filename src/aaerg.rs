8aa9ac661b7364a08145dd50416055c9a2f2a08e
Cameron Lyons <cameron.lyons2@gmail.com>
2023-11-21 14:39:10 -0600

    aareg options

--- src/aaerg.rs
+++ src/aaerg.rs
@@ -1,6 +1,26 @@
 // Aalen’s additive regression model for censored data
 
+use ndarray::Array2;
+use std::collections::HashMap;
+
 enum NaAction {
     Fail,
     Exclude,
+}
+
+struct AaregOptions {
+    formula: String,                       // Formula as a string
+    data: Array2<f64>,                     // 2D array for the dataset
+    weights: Option<Vec<f64>>,             // Optional weights
+    subset: Option<Vec<usize>>,            // Optional subset of data indices
+    na_action: Option<NaAction>,           // Action for NA values
+    qrtol: f64,                            // Tolerance for singularity detection
+    nmin: Option<usize>,                   // Minimum number of observations
+    dfbeta: bool,                          // Whether to compute dfbeta residuals
+    taper: f64,                            // Taper parameter
+    test: Vec<String>,                     // List of tests to perform
+    cluster: Option<HashMap<String, i32>>, // Optional clustering
+    model: bool,                           // Whether to include the model frame in the output
+    x: bool,                               // Whether to include the matrix of predictors
+    y: bool,                               // Whether to include the response vector
 }
