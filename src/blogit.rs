e2d76a0a4fa69da987109882ec5c8fe2a1f894c0
Cameron Lyons <cameron.lyons2@gmail.com>
2023-11-24 09:55:29 -0600

    bprobit

--- src/blogit.rs
+++ src/blogit.rs
@@ -18,4 +18,15 @@ impl LinkFunctionParams {
         };
         adjusted_input.ln() - (1.0 - adjusted_input).ln()
     }
+
+    fn bprobit(&self, input: f64) -> f64 {
+        let adjusted_input = if input < self.edge {
+            self.edge
+        } else if input > 1.0 - self.edge {
+            1.0 - self.edge
+        } else {
+            input
+        };
+        adjusted_input.probit() - (1.0 - adjusted_input).probit()
+    }
 }
