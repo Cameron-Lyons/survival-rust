307ba5df9f6665313dccd75e632afb7606ae04d7
Cameron Lyons <cameron.lyons2@gmail.com>
2023-11-24 09:50:03 -0600

    blogit

--- src/blogit.rs
+++ src/blogit.rs
@@ -8,4 +8,14 @@ impl LinkFunctionParams {
     fn new(edge: f64) -> Self {
         LinkFunctionParams { edge }
     }
+    fn blogit(&self, input: f64) -> f64 {
+        let adjusted_input = if input < self.edge {
+            self.edge
+        } else if input > 1.0 - self.edge {
+            1.0 - self.edge
+        } else {
+            input
+        };
+        adjusted_input.ln() - (1.0 - adjusted_input).ln()
+    }
 }
