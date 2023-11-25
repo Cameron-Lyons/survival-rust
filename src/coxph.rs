6b3a0c1a44e8e19706b3d53ecf7401861a7ddf96
Cameron Lyons <cameron.lyons2@gmail.com>
2023-11-25 14:40:34 -0600

    new

--- src/coxph.rs
+++ src/coxph.rs
@@ -10,3 +10,16 @@ struct CoxPHModel {
     censoring: Vec<u8>,
     covariates: Array2<f64>,
 }
+
+impl CoxPHModel {
+    pub fn new(covariates: Array2<f64>, event_times: Vec<f64>, censoring: Vec<u8>) -> Self {
+        Self {
+            coefficients: Array2::<f64>::zeros((covariates.ncols(), 1)),
+            baseline_hazard: Vec::new(),
+            risk_scores: Vec::new(),
+            event_times,
+            censoring,
+            covariates,
+        }
+    }
+}
