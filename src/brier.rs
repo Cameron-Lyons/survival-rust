6ec1883122fdd4710f378d4bb238c610617728de
Cameron Lyons <cameron.lyons2@gmail.com>
2023-11-25 20:47:33 -0600

    brier

--- src/brier.rs
+++ src/brier.rs
@@ -1,1 +1,17 @@
 // Compute the Brier score for a Cox model
+
+use coxph::CoxPHModel;
+
+impl CoxPHModel {
+    /// Compute the Brier score for the model
+    pub fn brier_score(&self) -> f64 {
+        let mut score = 0.0;
+        let mut count = 0.0;
+        for (time, status) in self.data.iter() {
+            let pred = self.predict(time);
+            score += (pred - status).powi(2);
+            count += 1.0;
+        }
+        score / count
+    }
+}
