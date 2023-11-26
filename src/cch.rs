cc5042f2d44d80bee2f60215b4db2b4047aeecd0
Cameron Lyons <cameron.lyons2@gmail.com>
2023-11-25 22:55:34 -0600

    subjects

--- src/cch.rs
+++ src/cch.rs
@@ -7,3 +7,15 @@ enum Method {
     IBorgan,
     IIBorgan,
 }
+
+struct Subject {
+    id: usize,            // Unique identifier for each subject
+    covariates: Vec<f64>, // Covariates (e.g., age, blood pressure, etc.)
+    is_case: bool,        // True if the subject is a case, false if a control
+    is_subcohort: bool,   // True if the subject is in the sub-cohort
+    stratum: usize,       // Stratum identifier, if applicable
+}
+
+struct CohortData {
+    subjects: Vec<Subject>, // Collection of all subjects
+}
