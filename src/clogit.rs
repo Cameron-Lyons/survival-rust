74f83cc7ffe7452b76942d662eb01a458578fe44
Cameron Lyons <cameron.lyons2@gmail.com>
2023-11-26 22:37:54 -0600

    fit

--- src/clogit.rs
+++ src/clogit.rs
@@ -39,4 +39,60 @@ impl DataSet {
 struct ConditionalLogisticRegression {
     data: DataSet,
     coefficients: Vec<f64>,
+    max_iter: u32,
+    tol: f64,
+}
+
+impl ConditionalLogisticRegression {
+    pub fn new(data: DataSet) -> ConditionalLogisticRegression {
+        ConditionalLogisticRegression {
+            data,
+            coefficients: Vec::new(),
+            max_iter: 100,
+            tol: 1e-6,
+        }
+    }
+    pub fn set_max_iter(&mut self, max_iter: u32) {
+        self.max_iter = max_iter;
+    }
+    pub fn set_tol(&mut self, tol: f64) {
+        self.tol = tol;
+    }
+    pub fn fit(&mut self) {
+        let num_covariates = self.data.get_num_covariates();
+        self.coefficients = vec![0.0; num_covariates];
+        let mut old_coefficients = vec![0.0; num_covariates];
+        let mut iter = 0;
+        while iter < self.max_iter {
+            for covariate in 0..num_covariates {
+                let mut numerator = 0.0;
+                let mut denominator = 0.0;
+                for observation in 0..self.data.get_num_observations() {
+                    let case_control_status = self.data.get_case_control_status(observation);
+                    let stratum = self.data.get_stratum(observation);
+                    let covariates = self.data.get_covariates(observation);
+                    let mut exp_sum = 0.0;
+                    for covariate in 0..num_covariates {
+                        exp_sum += self.coefficients[covariate] * covariates[covariate];
+                    }
+                    let exp = exp_sum.exp();
+                    numerator += case_control_status as f64 * covariates[covariate] * exp;
+                    denominator += covariates[covariate] * exp;
+                }
+                old_coefficients[covariate] = self.coefficients[covariate];
+                self.coefficients[covariate] += numerator / denominator;
+            }
+            let mut diff = 0.0;
+            for covariate in 0..num_covariates {
+                diff += (self.coefficients[covariate] - old_coefficients[covariate]).abs();
+            }
+            if diff < self.tol {
+                break;
+            }
+            iter += 1;
+        }
+    }
+    pub fn get_coefficients(&self) -> &Vec<f64> {
+        &self.coefficients
+    }
 }
