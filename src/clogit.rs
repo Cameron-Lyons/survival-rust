6df775ee83b3cd0b3f50234817fc92bc9e2ed7cf
Cameron Lyons <cameron.lyons2@gmail.com>
2023-11-26 22:05:10 -0600

    traits

--- src/clogit.rs
+++ src/clogit.rs
@@ -5,3 +5,33 @@ struct DataSet {
     strata: Vec<u8>,
     covariates: Vec<Vec<f64>>,
 }
+
+impl DataSet {
+    pub fn new() -> DataSet {
+        DataSet {
+            case_control_status: Vec::new(),
+            strata: Vec::new(),
+            covariates: Vec::new(),
+        }
+    }
+    pub fn add_observation(&mut self, case_control_status: u8, stratum: u8, covariates: Vec<f64>) {
+        self.case_control_status.push(case_control_status);
+        self.strata.push(stratum);
+        self.covariates.push(covariates);
+    }
+    pub fn get_case_control_status(&self, id: usize) -> u8 {
+        self.case_control_status[id]
+    }
+    pub fn get_stratum(&self, id: usize) -> u8 {
+        self.strata[id]
+    }
+    pub fn get_covariates(&self, id: usize) -> &Vec<f64> {
+        &self.covariates[id]
+    }
+    pub fn get_num_observations(&self) -> usize {
+        self.case_control_status.len()
+    }
+    pub fn get_num_covariates(&self) -> usize {
+        self.covariates[0].len()
+    }
+}
