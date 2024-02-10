b9d203dc66c699f8aa66e6133bcaeba36a0e99f7
Cameron Lyons <cameron.lyons2@gmail.com>
2024-02-10 11:25:17 -0600

    update crates

---
+++ target/debug/build/num-integer-22e9d7169b34f0c7/build_script_build-22e9d7169b34f0c7.d
@@ -0,0 +1,5 @@
+/home/cameron/survival/target/debug/build/num-integer-22e9d7169b34f0c7/build_script_build-22e9d7169b34f0c7: /home/cameron/.cargo/registry/src/index.crates.io-6f17d22bba15001f/num-integer-0.1.45/build.rs
+
+/home/cameron/survival/target/debug/build/num-integer-22e9d7169b34f0c7/build_script_build-22e9d7169b34f0c7.d: /home/cameron/.cargo/registry/src/index.crates.io-6f17d22bba15001f/num-integer-0.1.45/build.rs
+
+/home/cameron/.cargo/registry/src/index.crates.io-6f17d22bba15001f/num-integer-0.1.45/build.rs:
---
+++ target/debug/build/pyo3-build-config-d8ed80915cd0e889/build_script_build-d8ed80915cd0e889.d
@@ -0,0 +1,9 @@
+/home/cameron/survival/target/debug/build/pyo3-build-config-d8ed80915cd0e889/build_script_build-d8ed80915cd0e889: /home/cameron/.cargo/registry/src/index.crates.io-6f17d22bba15001f/pyo3-build-config-0.20.2/build.rs /home/cameron/.cargo/registry/src/index.crates.io-6f17d22bba15001f/pyo3-build-config-0.20.2/src/impl_.rs /home/cameron/.cargo/registry/src/index.crates.io-6f17d22bba15001f/pyo3-build-config-0.20.2/src/errors.rs
+
+/home/cameron/survival/target/debug/build/pyo3-build-config-d8ed80915cd0e889/build_script_build-d8ed80915cd0e889.d: /home/cameron/.cargo/registry/src/index.crates.io-6f17d22bba15001f/pyo3-build-config-0.20.2/build.rs /home/cameron/.cargo/registry/src/index.crates.io-6f17d22bba15001f/pyo3-build-config-0.20.2/src/impl_.rs /home/cameron/.cargo/registry/src/index.crates.io-6f17d22bba15001f/pyo3-build-config-0.20.2/src/errors.rs
+
+/home/cameron/.cargo/registry/src/index.crates.io-6f17d22bba15001f/pyo3-build-config-0.20.2/build.rs:
+/home/cameron/.cargo/registry/src/index.crates.io-6f17d22bba15001f/pyo3-build-config-0.20.2/src/impl_.rs:
+/home/cameron/.cargo/registry/src/index.crates.io-6f17d22bba15001f/pyo3-build-config-0.20.2/src/errors.rs:
+
+# env-dep:CARGO_PKG_VERSION=0.20.2
