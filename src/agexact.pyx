fdd6ea3a2624f1a0478e9761802789085ea2f3a7
Cameron Lyons <cameron.lyons2@gmail.com>
2024-03-28 21:45:02 -0500

    agexact def

--- src/agexact.pyx
+++ src/agexact.pyx
@@ -5,3 +5,26 @@ cdef extern from "math.h":
 cdef extern from "survS.h":
 
 cdef extern from "survproto.h":
+
+
+cdef void agexact(int* maxiter, int* nusedx, int* nvarx, double* start, 
+                  double* stop, int* event, double* covar2, double* offset, 
+                  int* strata, double* means, double* beta, double* u, 
+                  double* imat2, double loglik[2], int* flag, double* work, 
+                  int* work2, double* eps, double* tol_chol, 
+                  double* sctest, int* nocenter):
+    cdef:
+        int i, j, k, l, person
+        int iter
+        int n, nvar
+        double **covar, **cmat, **imat  # Ragged array versions
+        double *a, *newbeta
+        double *score, *newvar
+        double denom, zbeta, weight
+        double time
+        double temp
+        double newlk = 0
+        int halving
+        int nrisk, deaths
+        int *index, *atrisk
+
