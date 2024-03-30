05d5fd27da60f39c69f1f5a063876929ac48d0f2
Cameron Lyons <cameron.lyons2@gmail.com>
2024-03-29 22:25:21 -0500

    func def

--- src/agexact.pyx
+++ src/agexact.pyx
@@ -1,23 +1,29 @@
-cdef extern from "math.h":
-    double exp(double x)
-    double log(double x)
+from libc.math cimport exp, log
+import numpy as np
+cimport numpy as cnp
 
 cdef extern from "survS.h":
+    double** dmatrix(double* data, int nrows, int ncols)
 
 cdef extern from "survproto.h":
+    int cholesky2(double** mat, int n, double tol)
+    void chsolve2(double** mat, int n, double* b)
+    void chinv2(double** mat, int n)
+    int init_doloop(int start, int end)
+    int doloop(int size, int* index)
 
-
-cdef void agexact(int* maxiter, int* nusedx, int* nvarx, double* start, 
-                  double* stop, int* event, double* covar2, double* offset, 
-                  int* strata, double* means, double* beta, double* u, 
-                  double* imat2, double loglik[2], int* flag, double* work, 
-                  int* work2, double* eps, double* tol_chol, 
-                  double* sctest, int* nocenter):
+cpdef void agexact(int* maxiter, int* nusedx, int* nvarx, double* start, 
+                   double* stop, int* event, double* covar2, double* offset, 
+                   int* strata, double* means, double* beta, double* u, 
+                   double* imat2, double loglik[2], int* flag, double* work, 
+                   int* work2, double* eps, double* tol_chol, 
+                   double* sctest, int* nocenter):
     cdef:
         int i, j, k, l, person
         int iter
-        int n, nvar
-        double **covar, **cmat, **imat  # Ragged array versions
+        int n = nusedx[0]
+        int nvar = nvarx[0]
+        double **covar, **cmat, **imat
         double *a, *newbeta
         double *score, *newvar
         double denom, zbeta, weight
