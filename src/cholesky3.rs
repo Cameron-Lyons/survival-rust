bc33475e7251f79dd6b4ff611da8fd37c4ea0c04
Cameron Lyons <cameron.lyons2@gmail.com>
2025-01-30 08:57:08 -0600

    cholesky3

---
+++ src/cholesky3.rs
@@ -0,0 +1,75 @@
+fn cholesky3(matrix: &mut [&mut [f64]], n: usize, m: usize, diag: &[f64], toler: f64) -> i32 {
+    let n2 = n - m;
+    let mut nonneg = 1;
+    let mut eps = 0.0;
+
+    // Compute the initial epsilon based on diagonal elements
+    for &val in diag.iter().take(m) {
+        if val < eps {
+            eps = val;
+        }
+    }
+    for i in 0..n2 {
+        let val = matrix[i][i + m];
+        if val < eps {
+            eps = val;
+        }
+    }
+
+    if eps == 0.0 {
+        eps = toler;
+    } else {
+        eps *= toler;
+    }
+
+    let mut rank = 0;
+
+    for i in 0..m {
+        let pivot = diag[i];
+        if !pivot.is_finite() || pivot < eps {
+            for j in 0..n2 {
+                matrix[j][i] = 0.0;
+            }
+            if pivot < -8.0 * eps {
+                nonneg = -1;
+            }
+        } else {
+            rank += 1;
+            for j in 0..n2 {
+                let temp = matrix[j][i] / pivot;
+                matrix[j][i] = temp;
+                let update = temp * temp * pivot;
+                matrix[j][j + m] -= update;
+                for k in (j + 1)..n2 {
+                    matrix[k][j + m] -= temp * matrix[k][i];
+                }
+            }
+        }
+    }
+
+    // Process the dense portion (remaining columns)
+    for i in 0..n2 {
+        let pivot = matrix[i][i + m];
+        if !pivot.is_finite() || pivot < eps {
+            for j in i..n2 {
+                matrix[j][i + m] = 0.0;
+            }
+            if pivot < -8.0 * eps {
+                nonneg = -1;
+            }
+        } else {
+            rank += 1;
+            for j in (i + 1)..n2 {
+                let temp = matrix[j][i + m] / pivot;
+                matrix[j][i + m] = temp;
+                let update = temp * temp * pivot;
+                matrix[j][j + m] -= update;
+                for k in (j + 1)..n2 {
+                    matrix[k][j + m] -= temp * matrix[k][i + m];
+                }
+            }
+        }
+    }
+
+    rank * nonneg
+}
