f4212d47d9046a94570b18e4285c7da8e554561d
Cameron Lyons <cameron.lyons2@gmail.com>
2025-02-19 11:01:54 -0600

    dmatrix

---
+++ src/dmatrix.rs
@@ -0,0 +1,19 @@
+pub fn dmatrix(array: &[f64], nrow: usize, ncol: usize) -> Vec<&[f64]> {
+    let mut columns = Vec::with_capacity(ncol);
+    for col in 0..ncol {
+        let start = col * nrow;
+        let end = start + nrow;
+        columns.push(&array[start..end]);
+    }
+    columns
+}
+
+pub fn imatrix(array: &[i32], nrow: usize, ncol: usize) -> Vec<&[i32]> {
+    let mut columns = Vec::with_capacity(ncol);
+    for col in 0..ncol {
+        let start = col * nrow;
+        let end = start + nrow;
+        columns.push(&array[start..end]);
+    }
+    columns
+}
