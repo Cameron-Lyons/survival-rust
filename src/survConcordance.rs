a11c2cc2c1725546a32d16c206e1406ed508f363
Cameron Lyons <cameron.lyons2@gmail.com>
2025-02-28 09:59:14 -0600

    survcon

--- src/survConcordance.rs
+++ src/survConcordance.rs
@@ -1,76 +1,100 @@
-use pyo3::prelude::*;
-
-#[pyfunction]
-fn surv_concordance(
-    np: usize,
+pub fn surv_concordance(
+    n: usize,
     time: &[f64],
     status: &[i32],
     x: &[f64],
-    n2p: usize,
+    n2: usize,
     x2: &[f64],
     temp: &mut [i32],
     result: &mut [i32],
 ) {
-    let (count1, count2) = temp.split_at_mut(n2p);
-    let n = np;
-    let n2 = n2p;
-
-    for i in result.iter_mut() {
-        *i = 0;
+    for r in result.iter_mut() {
+        *r = 0;
     }
 
-    for count in count1.iter_mut() {
-        *count = 0;
+    let (count1, count2) = temp.split_at_mut(n2);
+
+    for c in count1.iter_mut() {
+        *c = 0;
     }
+
+    let mut tdeath = 0;
 
     for i in 0..n {
-        let mut tdeath = 0;
-        let mut nright = 0;
-        let mut nsame = 0;
+        if status[i] > 0 {
+            let count = if tdeath == 0 { count1 } else { count2 };
 
-        if status[i] != 0 {
-            tdeath = 1;
-            nsame = 1;
-
-            for j in (i + 1)..n {
-                if time[j] < time[i] {
-                    break;
-                }
-                if status[j] != 0 {
-                    nsame += 1;
-                    if time[j] == time[i] {
-                        tdeath += 1;
-                    }
-                }
-            }
-
-            let mut k = (n2 - 1) / 2;
+            let mut nright = 0;
             let mut start = 0;
             let mut end = n2 - 1;
+            let mut k = 0;
 
-            count2.copy_from_slice(count1);
-
-            while start < end {
-                if x[i] < x2[k] {
+            while start <= end {
+                k = (start + end) / 2;
+                if x[i] == x2[k] {
+                    break;
+                } else if x[i] < x2[k] {
                     end = k - 1;
-                    k = (start + end) / 2;
-                } else {
-                    nright += count1[k] - count2[k];
-                    if x[i] > x2[k] {
-                        start = k + 1;
-                        k = (start + end + 1) / 2;
-                    } else {
-                        break;
+                    let right_start = k + 1;
+                    let right_end = end;
+                    if right_start <= right_end {
+                        let mid = (right_start + right_end) / 2;
+                        nright += count[mid];
                     }
+                } else {
+                    start = k + 1;
                 }
             }
 
-            count2[k] += nsame;
-            result[0] += nsame * (count1[k] - count2[k]);
-            result[1] += nsame * nright;
-            result[2] += tdeath * (n - i - nsame);
-            result[3] += tdeath * (nright + count1[k] - count2[k]);
-            result[4] += tdeath * (i - nright - count1[k]);
+            let mut nsame = count[k];
+
+            if k < end {
+                let right_start = k + 1;
+                let right_end = end;
+                let mid = (right_start + right_end) / 2;
+                let j = count[mid];
+                nsame -= j;
+                nright += j;
+            }
+
+            if k > start {
+                let left_start = start;
+                let left_end = k - 1;
+                let mid = (left_start + left_end) / 2;
+                let j = count[mid];
+                nsame -= j;
+            }
+
+            result[3] += nsame;
+            result[1] += nright;
+            result[0] += (i - tdeath) - (nsame + nright);
+
+            if i < n - 1 && status[i + 1] > 0 && time[i] == time[i + 1] {
+                tdeath += 1;
+                if tdeath == 1 {
+                    count2.copy_from_slice(count1);
+                }
+            } else {
+                result[2] += (tdeath * (tdeath + 1)) / 2;
+                tdeath = 0;
+            }
+        } else {
+            tdeath = 0;
+            result[4] += i;
+        }
+
+        let mut start = 0;
+        let mut end = n2 - 1;
+        while start <= end {
+            let k = (start + end) / 2;
+            count1[k] += 1;
+            if x[i] == x2[k] {
+                break;
+            } else if x[i] < x2[k] {
+                end = k - 1;
+            } else {
+                start = k + 1;
+            }
         }
     }
 }
